aad8c869ebbd4e6aa9b0c766dfa161fb4349b97b
nza34 <100048596+nza34@users.noreply.github.com>
2026-04-26 00:29:53 -0700

    Implemented BattleScheduler with Board helper functions

--- Assets/Scripts/Data/Player.cs
+++ Assets/Scripts/Data/Player.cs
@@ -7,12 +7,5 @@ public class Player : ScriptableObject
     public float MaxHealth{get; private set;}
     public string Name{get; private set;}
     public List<Relic> RelicList = new List<Relic>();
-    public float BaseBlock{get; private set;}
-    
-    //then a constructor for the data Layer
-    public Player(float maxHealth, string name, float baseBlock){
-        MaxHealth = maxHealth;
-        Name = name;    
-        BaseBlock = baseBlock;
-    }
+    public float Block{get; private set;}
 }
--- Assets/Scripts/Logic/Board.cs
+++ Assets/Scripts/Logic/Board.cs
@@ -24,8 +24,31 @@ public class Board : MonoBehaviour
     private GameObject[,] backgroundTiles;
     private FindMatches findMatches;
 
-    
+    public BattleScheduler battleScheduler;
 
+    public TurnResult turnResult;
+
+    // call this before destroying
+    public TurnResult TallyMatches()
+    {
+        TurnResult result = default;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                var go = allTileInstances[i, j];
+                if (go != null && go.GetComponent<TileInstance>().isMatched)
+                {
+                    Tile tileData = go.GetComponent<TileInstance>().tileData;
+                    // BattleScheduler resolves what the tile means
+                    result.Add(battleScheduler.ResolveTile(tileData, 1));
+                }
+            }
+        }
+        return result;
+    }
+
+    
     void Start()
     {
         findMatches = FindAnyObjectByType<FindMatches>();
@@ -122,20 +145,29 @@ public class Board : MonoBehaviour
 
     private void DestroyMatchesAt(int column, int row)
     {
+        
+        
         if (allTileInstances[column, row].GetComponent<TileInstance>().isMatched)
         {
             GameObject particle = Instantiate(destroyParticle,
                 allTileInstances[column, row].transform.position,
                 Quaternion.identity);
+            
+
+
             Destroy(particle, .5f);
 
             Destroy(allTileInstances[column, row]);
             allTileInstances[column, row] = null;
+            
         }
     }
 
     public void DestroyMatches()
     {
+        TurnResult waveResult = TallyMatches();
+        battleScheduler.AccumulateWave(waveResult);
+        
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -233,6 +265,10 @@ public class Board : MonoBehaviour
         {
             yield return new WaitForSeconds(.5f);
 
+            // handles tallying for the Board.
+            TurnResult waveResult = TallyMatches();
+            battleScheduler.AccumulateWave(waveResult);
+
             for (int i = 0; i < width; i++)
                 for (int j = 0; j < height; j++)
                     if (allTileInstances[i, j] != null)
@@ -248,6 +284,7 @@ public class Board : MonoBehaviour
             findMatches.FindAllMatches();
             yield return new WaitForSeconds(.3f);
         }
+        battleScheduler.ResolveTurn();
 
         findMatches.currentMatches.Clear();
         currentTile = null;
